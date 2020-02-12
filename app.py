f7927dc25f993d943e3f875698c0b306e27d6721
shannonzylstra <shannonzylstra@gmail.com>
2020-02-12 09:28:02 -0800

    Add recipe GET and POST routes

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 # Import needed packages
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 
 # Declare app instance
 app = Flask(__name__)
+
+app.data = {
+    'pie': {
+        'name': 'Strawberry Rhubarb Pie',
+        'ingredients': [
+            'strawberries',
+            'rhubarb',
+            'piecrust'
+        ]
+    }
+}
 
 # Declare app routes
 @app.route('/')
@@ -11,14 +22,20 @@ def home():
 
 @app.route('/greeting')
 def greeting():
-    return render_template('index.html', name='Rachel Maddow')
+    return render_template('greetings.html', name='Rachel Maddow')
 
 @app.route('/pie')
 def pie():
-    ingredients = [
-        'strawberries',
-        'rhubarb',
-        'piecrust'
-    ]
-    return jsonify({'pie ingredient': ingredients[1]})
-    
+    return jsonify({'pie ingredient': app.data['pie']['ingredients'][1]})
+
+@app.route('/recipe', methods=('GET', 'POST'))
+def recipe():
+    if request.method == 'GET':
+        #  Render a template that shows the name of a pie, all ingredients, and a new ingredient form
+        return render_template('recipe.html', pie=app.data['pie'])
+    elif request.method == 'POST':
+        # The form from the GET page sends a new ingredient. Add this to your ingredients list then redirect back to `/recipe`
+        # Assume we are accepting form data
+        ing = str(request.form['ing'])
+        app.data['pie']['ingredients'].append(ing)
+        return redirect('/recipe')
